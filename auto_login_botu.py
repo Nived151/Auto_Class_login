40ec7418eb91476f37fa6da577b414b3a1e6fb18
Nived151 <sainived1511@gmail.com>
2021-06-28 00:57:51 +0530

    sandbox

--- auto_login_botu.py
+++ auto_login_botu.py
@@ -23,6 +23,7 @@ chrome_options.add_argument("--disable-infobars")
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--start-maximized")
+chrome_options.addArguments("--no-sandbox")
 chrome_options.add_experimental_option("prefs", { \
     "profile.default_content_setting_values.media_stream_mic": 1, 
     "profile.default_content_setting_values.media_stream_camera": 1,
